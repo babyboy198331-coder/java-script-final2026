6fe4ae391fedfa9d3a2fe0710f770fb50bdb5290
babyboy198331-coder <babyboy198331@gmail.com>
2026-04-03 12:36:03 -0500

    master

--- user.js
+++ user.js
@@ -1,13 +1,27 @@
+const postListEl = document.querySelector('')
 async function main() {
     const id = localStorage.getItem("id");
     const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
     const postData = await posts.json();
     console.log(postData);
 
-    postData.map((post) => {
-        // process each post
-        console.log(post);
-    });
+    postListEl.innerHTML = postData.map((post) => `
+         <div class="post">
+      <div class="post__title">
+        ${post.title}
+      </div>
+      <p class="post__body">
+        ${post.body}
+      </p>
+    </div>
+`).join("");
+
+    const container = document.getElementById('posts');
+    if (container) {
+        container.innerHTML = html;
+    } else {
+        console.log('No container element with id "posts" found.');
+    }
 }
 
 main();
