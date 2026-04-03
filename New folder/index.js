cd84ee6dabcd1fb512be8abd5a367d61b870f0c1
babyboy198331-coder <babyboy198331@gmail.com>
2026-04-03 09:50:17 -0500

    master

--- New folder/index.js
+++ New folder/index.js
@@ -1,2 +1,20 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+
+async function main(){
+    const users = await fetch("https://jsonplaceholder.typicode.com/users");
+    const userData = await users.json();
+    const userListEl = document.querySelector(".user-list");
+    userData.map(user => {
+    const html = `<div class="user-card">
+            <div class="user-card__container">
+              <h3>User's Name</h3>
+                <p><b>Email:</b> email@email.com</p>
+                <p><b>Phone:</b> 0000000000</p>
+                <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+            </div>
+          </div>`;
+    });
+}
+
+main();
